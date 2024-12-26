a6be58aa904e52aa0240ff9c2d339b0d2aac9953
tehmi khan <tehmikhan32@gmail.com>
2024-12-26 21:39:25 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+# Airthmatic Operator
+# Addition Operator (+)
+a = 7
+b = 12
+print (a + b)
+# Output : 19
+# Substraction Operator (-)
+a = 10
+b = 25
+print (b - a)
+# output : 15
+# Division Operator (/)
+a = 8
+b = 3
+print (8 / 3)
+#Output : 2.66
+# Multiplication Operator (*)
+a = 28
+b = 6
+print (a * b)
+# Output : 168
+# Floor Division (//)
+a = 15
+b = 11
+print (a // b)
+# Output : 1
+# Modulus operator (%)
+a = 20
+b = 7
+print (a % b)
+# Output : 6
+# Exponentiation Operator (**)
+a = 12
+b = 3
+print (a ** b)
+# Output : 1728
+
+# Basic Assignment Operator
+x = 10
+# Addition  (+=)
+x = 10
+x += 5
+print (x)   # output: 15
+# Subtraction Assignment Operator (-=)
+x = 10      
+x -= 8
+print (x)   # Output: 2
+# Multiplication Assignment Operator (*=)
+x = 10      
+x *= 2
+print (x)   # Output: 20
+# Division Assignment Operator (/=)
+x = 10      
+x /= 6
+print (x)   # Output: 1.66
+# Floor Assignment Division (//=)
+x = 10      
+x //= 3
+print (x)   # Output: 3
+# Modulus Assignment operator (%=)
+x = 10      
+x %= 4
+print (x)   # Output: 2
+# Exponentiation Assignment Operator (**=)
+x = 10      
+x **= 4
+print (x)   # Output:10000
+
+
+
+
