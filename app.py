5fcbe582f91fd11dfb943ec2d31c5d3107cb0bc3
tehmi khan <tehmikhan32@gmail.com>
2025-01-09 06:18:46 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,71 +1,29 @@
-# Airthmatic Operator
-# Addition Operator (+)
-a = 7
-b = 12
-print (a + b)
-# Output : 19
-# Substraction Operator (-)
-a = 10
-b = 25
-print (b - a)
-# output : 15
-# Division Operator (/)
-a = 8
-b = 3
-print (8 / 3)
-#Output : 2.66
-# Multiplication Operator (*)
-a = 28
-b = 6
-print (a * b)
-# Output : 168
-# Floor Division (//)
-a = 15
-b = 11
-print (a // b)
-# Output : 1
-# Modulus operator (%)
-a = 20
-b = 7
-print (a % b)
-# Output : 6
-# Exponentiation Operator (**)
-a = 12
-b = 3
-print (a ** b)
-# Output : 1728
-
-# Basic Assignment Operator
-x = 10
-# Addition  (+=)
-x = 10
-x += 5
-print (x)   # output: 15
-# Subtraction Assignment Operator (-=)
-x = 10      
-x -= 8
-print (x)   # Output: 2
-# Multiplication Assignment Operator (*=)
-x = 10      
-x *= 2
-print (x)   # Output: 20
-# Division Assignment Operator (/=)
-x = 10      
-x /= 6
-print (x)   # Output: 1.66
-# Floor Assignment Division (//=)
-x = 10      
-x //= 3
-print (x)   # Output: 3
-# Modulus Assignment operator (%=)
-x = 10      
-x %= 4
-print (x)   # Output: 2
-# Exponentiation Assignment Operator (**=)
-x = 10      
-x **= 4
-print (x)   # Output:10000
-
-
-
-
+import calendar
+year = 2025
+month = 1
+print (calendar.month(year,month))
+#  January 2025
+# Mo Tu We Th Fr Sa Su
+#       1  2  3  4  5
+# 6  7  8  9 10 11 12
+# 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26
+# 27 28 29 30 31
+table = 2
+for table in range (1,11):
+    print(f"2x{table}=",table*2)
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10= 20
+square =lambda x: x**2
+print(square (20))
+print(square(15))
+# 400
+# 225
