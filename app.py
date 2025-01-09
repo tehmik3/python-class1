8904f11bbd3c083d184180476610ccb92a731dad
tehmi khan <tehmikhan32@gmail.com>
2025-01-09 06:51:18 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ print (calendar.month(year,month))
 # 20 21 22 23 24 25 26
 # 27 28 29 30 31
 table = 2
-for table in range (1,11):
+for table in range (10):
     print(f"2x{table}=",table*2)
 # 2 x 1 = 2
 # 2 x 2 = 4
@@ -21,7 +21,34 @@ for table in range (1,11):
 # 2 x 7 = 14
 # 2 x 8 = 16
 # 2 x 9 = 18
-# 2 x 10= 20
+# 2 x 10 = 20
+table = 3
+for table in range (11):
+    print(f"3x{table}=",table*3)
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# 3 x 4 = 12
+# 3 x 5 = 15
+# 3 x 6 = 18
+# 3 x 7 = 21
+# 3 x 8 = 24
+# 3 x 9 = 27
+# 3 x 10 = 30
+table = 4
+for table in range (11):
+    print(f"4x{table}=",table*4)
+# 4 x 1 = 4
+# 4 x 2 = 8
+# 4 x 3 = 12
+# 4 x 4 = 16
+# 4 x 5 = 20
+# 4 x 6 = 24
+# 4 x 7 = 28
+# 4 x 8 = 32
+# 4 x 9 = 36
+# 4 x 10 = 40
+
 square =lambda x: x**2
 print(square (20))
 print(square(15))
