7e47b3d6a4b42b2f5f681d500ed15996a9b5c20c
tehmi khan <tehmikhan32@gmail.com>
2025-01-10 02:07:48 +0500

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,11 @@
+import calendar
+year = 2025
+month = 10
+print (calendar.month(year,month))
+#   October 2025
+# Mo Tu We Th Fr Sa Su
+#       1  2  3  4  5
+# 6  7  8  9 10 11 12
+# 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26
+# 27 28 29 30 31
