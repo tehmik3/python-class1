2ffe210943ffe30ccaa806bf472c185d0feebc69
tehmi khan <tehmikhan32@gmail.com>
2025-01-10 02:24:37 +0500

    Add files via upload

---
+++ airthmatic.py
@@ -0,0 +1,25 @@
+
+#  Revision of Previous Classes
+#  Python install
+#  CMD | python--version | python|
+#  print ("hello world")
+#  vscode |creat python folder| vscode file-open folder-then select new file
+#  print("hi tehmi!")
+#  search bar clear| type CMD
+#  veriable = to store data name: str = "andrew"
+#  age: int = 30
+#  isActive: bool = true
+#  print (name)
+#  comment (#)
+#  then Airthmatic operator
+x = 10
+y = 15
+print (x * y)  # 150
+print (x + y)  # 25
+print (x - y)  # -5
+print (x / y)  # 0.66
+print (x // y) # 0 
+print (x ** y) # 1000000000000000
+print (x % y)  # 10
+
+   
