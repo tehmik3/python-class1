1d18c5a4e9c07357a93b74d818edbbbf0e715e46
tehmi khan <tehmikhan32@gmail.com>
2025-01-13 11:55:01 +0500

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,47 @@
+# num = 1 
+# num = num +1
+# print ("num first time", num)
+# num = num + 1
+# print ("num second time", num)
+# # num first time 2
+# # num second time 3
+# string
+# intiger
+# boolean
+# list 
+# tuple
+# numbers = [1,2,3,4,5]
+# #          1     ,  2   ,   3     ,   4    ,  5     ,   6          (index)
+names = ["tahreem","dua","mariyam","rimsha","mahira","aimon"]    # (list)
+# print (names[0])
+#  tahreem
+names1 = ["tahreem","dua","mariyam","rimsha","mahira","aimon"]    # (list)
+names2 = ("uroosa","dua","mariyam","rimsha","mahira","aimon")    # (tuple)
+name: str = "uroosa"
+name = "tahreem"
+print ("names1 before overide", names1)
+#  uroosa = "tahreem"
+names1 [0] ="uroosa"
+print ("names1 after overide", names1)
+# names1 before overide ['tahreem', 'dua', 'mariyam', 'rimsha', 'mahira', 'aimon']
+# names1 after overide ['uroosa', 'dua', 'mariyam', 'rimsha', 'mahira', 'aimon']
+print ("names1 is", names1)
+print ("names2 is", names2)
+names1 is ['tahreem', 'dua', 'mariyam', 'rimsha', 'mahira', 'aimon']
+names2 is ('tahreem', 'dua', 'mariyam', 'rimsha', 'mahira', 'aimon')
+print ("list first name is", names1[0])
+print ("tuple first name is", names2[0])
+# list first name is tahreem
+# tuple first name is uroosa
+my_str = "python"
+x = 0
+for i in my_str:
+   x += 1
+   print(my_str [0:x])
+# p
+# py
+# pyt
+# pyth
+# pytho
+# python
+
